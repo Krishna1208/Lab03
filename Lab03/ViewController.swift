b3a1e69f078d38cfd61b73396233eca08545207b
Krishna1208 <Desaikrishna128@gmail.com>
2022-08-02 01:31:10 -0400

    Initial Commit

---
+++ Lab03/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Lab03
+//
+//  Created by Krishna Desai on 2022-07-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var searchText: UITextField!
+    @IBOutlet weak var imgWeather: UIImageView!
+    @IBOutlet weak var locLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet var conLabel: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onLocationTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func onSearchTapped(_ sender: UIButton) {
+    }
+}
+
